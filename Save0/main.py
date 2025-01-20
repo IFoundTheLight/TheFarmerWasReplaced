0e102d937642170fcbf81a88c45d2bf5f748cadf
Robert Fern <IFoundTheLight@Gmail.com>
2025-01-20 07:06:57 -0500

    1

--- Save0/main.py
+++ Save0/main.py
@@ -1,67 +1,64 @@
-# Imports
-from SmartPlant.py import *
-
+# Check if list contains item
 def ListContains(llist, item):
     for listItem in llist:
         if listItem == item:
             return True
-        
-# Clear Settings
-clear()
 
-#
-PlotsReadyForHarvest = 0
+# Main
+def Main():
 
-# Loop
-while True:
-
-    # Set World Size
-    WorldSize = get_world_size() * get_world_size()
-
-    SunflowerArea = [0, 1, get_world_size() + 2]
-
-    # Items
-    hayCount = num_items(Items.Hay)
-    carrotCount = num_items(Items.Carrot)
-    woodCount = num_items(Items.Wood)
-    pumpkinCount = num_items(Items.Pumpkin)
+    # Clear Settings
+    clear()
 
     #
-    HarvestOnlyOnFull = False
-    if hayCount <= 10000:
-        WhatToMake = Entities.Grass
-    elif woodCount <= 10000:
-        WhatToMake = Entities.Bush
-    elif carrotCount <= 10000:
-        WhatToMake = Entities.Carrot
-    elif pumpkinCount <= 10000:
-        WhatToMake = Entities.Pumpkin
-        HarvestOnlyOnFull = True
-    else:
-        WhatToMake = Entities.Grass
+    PlotsReadyForHarvest = 0
 
-    #
-    rowNum = 0
-    colNum = 0
+    # Loop
+    while True:
 
-    # Loop over Rows
-    for row in range(get_world_size()):
+        # Set World Size
+        WorldSize = get_world_size() * get_world_size()
 
-        # Loop over Cols
-        for col in range(get_world_size()):
+        #if Unlocks.Sunflowers and Unlocks.Lists:
+        #    SunflowerArea = [0, 1, get_world_size() + 2]
 
-            print('RowNum:', rowNum ,'ColNum: ', colNum)
+        # Items
+        hayCount = num_items(Items.Hay)
+        carrotCount = num_items(Items.Carrot)
+        woodCount = num_items(Items.Wood)
+        pumpkinCount = num_items(Items.Pumpkin)
 
-            #
-            field = rowNum * get_world_size() + colNum
-            if ListContains(SunflowerArea, field):
-                if get_ground_type() != Grounds.Soil:
-                    till()
-                if get_entity_type() != Entities.Sunflower:
-                    plant(Entities.Sunflower)  
+        #
+        HarvestOnlyOnFull = False
+        if hayCount <= 1000:
+            WhatToMake = Entities.Grass
+        elif woodCount <= 1000:
+            WhatToMake = Entities.Bush
+        elif carrotCount <= 1000:
+            WhatToMake = Entities.Carrot
+        elif pumpkinCount <= 1000:
+            WhatToMake = Entities.Pumpkin
+            HarvestOnlyOnFull = True
+        else:
+            WhatToMake = Entities.Grass
 
-            else:
-            
+        #
+        fieldNum = 0
+
+        # Loop over Rows
+        for row in range(get_world_size()):
+
+            # Loop over Cols
+            for col in range(get_world_size()):
+
+                #if Unlocks.Sunflowers and Unlocks.Lists and ListContains(SunflowerArea, field):
+                #    if get_ground_type() != Grounds.Soil:
+                #        till()
+                #    if get_entity_type() != Entities.Sunflower:
+                #        plant(Entities.Sunflower)  
+
+                #else:
+                
                 # What if needed
                 if num_items(Items.Water) > 0:
                     if get_water() < 0.5:
@@ -115,10 +112,11 @@ while True:
                     if get_entity_type() != Entities.Sunflower:
                         plant(Entities.Sunflower)         
 
-            # Move East
-            move(East)
-            colNum = colNum + 1
+                # Move East
+                move(East)
+                fieldNum = fieldNum + 1
 
-        # Move North
-        move(North)
-        rowNum = rowNum + 1
+            # Move North
+            move(North)
+
+Main()
